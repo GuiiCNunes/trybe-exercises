492102bc1dde7700c61f3eaed738390f7f739091
Guilherme Nunes <guiicnunes@gmail.com>
2021-03-24 18:52:25 -0300

    Pratica 5

---
+++ exercises/4.1/pract5.js
@@ -0,0 +1,18 @@
+let estado = 'aprovada(o)';
+
+switch (estado) {
+  case 'aprovada(o)':
+    console.log('Parabens, doida(o)!');
+    break;
+
+  case 'lista':
+    console.log('Ta listado, meu consagrado');
+    break;
+
+  case 'reprovada':
+    console.log('Força, meu bom. Estuda mais um pouco e dale');
+    break;
+
+  default:
+    console.log('Não se aplica');
+}
