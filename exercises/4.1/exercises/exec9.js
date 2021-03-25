1f9b0aad016f5e2a50ff307b17b76a3f06c635f2
Guilherme Nunes <guiicnunes@gmail.com>
2021-03-25 18:12:46 -0300

    Exec 9

---
+++ exercises/4.1/exercises/exec9.js
@@ -0,0 +1,9 @@
+let number1 = 2;
+let number2 = 1;
+let number3 = 2;
+
+if (number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0 ) {
+  console.log('True');
+} else {
+  console.log('False');
+}
