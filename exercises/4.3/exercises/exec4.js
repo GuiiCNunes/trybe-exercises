4714b19fd96454244591ca0fa6fcf890df35b596
Guilherme Nunes <guiicnunes@gmail.com>
2021-03-27 03:49:09 -0300

    Correcao Exec 4

--- exercises/4.3/exercises/exec4.js
+++ exercises/4.3/exercises/exec4.js
@@ -1,9 +1,10 @@
 let size = 5;
+let middle = Math.round(size / 2);
 
 for (let index = 0; index < size; index += 2) {
   let line = '';
-  let middle = Math.round(size / 2);
-  for (let indexQuantityChar = 0; indexQuantityChar <= size; indexQuantityChar += 1) {
+  
+  for (let indexQuantityChar = 1; indexQuantityChar <= size; indexQuantityChar += 1) {
     if (indexQuantityChar === middle || indexQuantityChar < middle + index && indexQuantityChar > middle - index) {
       line += '*';
     } else {
