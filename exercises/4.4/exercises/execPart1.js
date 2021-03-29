cf36c9172944ad36444ef270c3e125c230ef703f
Guilherme Nunes <guiicnunes@gmail.com>
2021-03-29 18:33:59 -0300

    Parte 1 exec 5

--- exercises/4.4/exercises/execPart1.js
+++ exercises/4.4/exercises/execPart1.js
@@ -19,3 +19,18 @@ console.log();
 for ( let key in info) {
   console.log(info[key]);
 }
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for (let key in info) {
+  if (key !== 'recorrente') {
+    console.log(info[key],'e',info2[key]);
+  } else {
+    console.log('Ambos recorrentes');
+  }
+}
