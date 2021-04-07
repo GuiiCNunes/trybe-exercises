b89e4cfbb11de31a2df9d69a4748ef4a802af3b7
Guilherme Nunes <guiicnunes@gmail.com>
2021-04-07 18:51:39 -0300

    Praticas do dia

---
+++ exercises/5.3/main.js
@@ -0,0 +1,54 @@
+const divUm = document.getElementById('divUm');
+const divDois = document.getElementById('divDois');
+const divTres = document.getElementById('divTres');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('mySpotrybefy');
+
+/*
+ Copie esse arquivo e edite apenas ele;
+ Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+ Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+com a classe 'tech';
+ Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+redirecione para alguma página;
+1. Que tal redirecionar para seu portifólio?
+ Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+a cor do mesmo;
+
+Segue abaixo um exemplo do uso de event.target:
+*/
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+divUm.addEventListener('dblclick', resetText);
+// Não precisa passar o parâmetro dentro do addEventListener. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'divUm'.
+
+function addClassTech(element) {
+  document.querySelector('.tech').classList.remove('tech');
+  element.className = 'tech';
+}
+input.addEventListener("keyup", addClassTech);
+
+// Implementar melhor
+function redirectPage(element) {
+  element.target.href = 'https://betrybe.com';
+}
+myWebpage.addEventListener("dblclick", redirectPage);
+
+function colorMagenta(element) {
+  element.target.style.color = 'magenta';
+}
+myWebpage.addEventListener("mouseover", colorMagenta);
+
+function colorWhite(element) {
+  element.target.style.color = 'white';
+}
+myWebpage.addEventListener("mouseout", colorWhite);
+
