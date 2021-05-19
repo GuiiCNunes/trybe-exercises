414d460ae718b96b23522e6c3a8c42f1bb69f980
Guilherme Nunes <guiicnunes@gmail.com>
2021-05-19 00:37:14 -0300

    Parte 1 Exec 4 Fazendo...

---
+++ exercises/10.1/exercises/part1exec4.spec.js
@@ -0,0 +1,13 @@
+const { myFizzBuzz } = require('./part1exec4.js');
+
+describe(`Testes funções myFizzBuzz()`, () => {
+  it(`Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado`, () => {
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz');
+  });
+  it(`Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado`, () => {
+    expect(myFizzBuzz(12)).toEqual('fizz');
+  });
+  it(`Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado`, () => {
+    expect(myFizzBuzz(10)).toEqual('buzz');
+  });
+});
