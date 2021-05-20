9d72c45fa51620ab8b71cc421cbb33c8c9c71125
Guilherme Nunes <guiicnunes@gmail.com>
2021-05-20 12:16:07 -0300

    Conteudo dia 10.2

--- exercises/10.2/pract/practPromise.spec.js
+++ exercises/10.2/pract/practPromise.spec.js
@@ -34,7 +34,7 @@ describe('Quando o tipo do animal existe', () => {
 describe('Quando o tipo do animal, não existe', () => {
   test('Retorne a lista de animais', () => {
     expect.assertions(1);
-    return getListAnimals('Dog').catch(error =>
+    return getListAnimals('Lion').catch(error =>
       expect(error).toEqual({ error: "Não possui esse tipo de animal." })
     );
   });
