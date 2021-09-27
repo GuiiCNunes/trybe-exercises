f25427e9710b199e4e6604c8ca72e2412f550c7a
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-27 20:04:09 -0300

    FEAT: Exec do dia

--- exercises/26.2/execs/exec4.js
+++ exercises/26.2/execs/exec4.js
@@ -19,17 +19,39 @@ async function viewAll() {
   }
 }
 
-function getIds() {
-  let ids = [];
-  getNames()
-    .then((array) => {
-      
-    })
-    .catch((err) => err);
-}
-
 function getName(id) {
   return new Promise((resolve, reject) => {
-    if
+    getNames()
+      .then((array) => {
+        const name = array.find((obj) => obj.id == id);
+        if (name === undefined) return reject( new Error("id não encontrado"));
+        return resolve(name)
+      });
   });
 }
+
+function excludePersons() {
+  getNames()
+    .then((arr) => {
+      return arr.filter((obj) => obj.id !== '10' && obj.id !== '6');
+    })
+    .then((data) => {
+      
+    })
+    .catch((err) => console.log(err));
+  
+  try {
+    await fs.writeFile('./meu-arquivo.txt', 'Meu textão');
+    console.log('Arquivo escrito com sucesso!');
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  }
+}
+
+function start() {
+  getName(3)
+    .then((obj) => console.log(`${obj.id} - ${obj.name}`))
+    .catch((err) => console.log(err));
+}
+
+start();
